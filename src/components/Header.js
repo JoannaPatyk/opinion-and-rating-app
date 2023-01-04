5d570726114bab82da7b2aefbe226acda718ded8
Joanna Patyk <patykjoanna93@gmail.com>
2023-01-04 14:32:51 +0100

    zmiany w stylach

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
-function Header({text, bgColor}) {
-    const headerStyles ={
-        backgroundColor: bgColor,
-    };
+import { VscFeedback } from "react-icons/vsc";
+
+function Header({ text, bgColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+  };
 
   return (
     <header style={headerStyles}>
       <div className="container">
-        <h2>{text}</h2>
+        <h2>
+          {text}
+          <VscFeedback />
+        </h2>
+
       </div>
     </header>
   )
